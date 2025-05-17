452864e981aa96d66ba1662b5919daa1d3a0f996
Jeremy Convocar <jac26@njit.edu>
2025-05-17 14:44:12 -0400

    more work done

---
+++ Assets/PlayerGrabberScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrabberScript : MonoBehaviour
+{
+    public Vector3 mousePos;
+
+    public float z_coord = -5;
+    public float max_z = 0;
+    public float min_z = -10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(Camera.main.ScreenToWorldPoint(Input.mousePosition).ToString());
+        Vector3 mouse = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        RaycastHit hit;
+        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+        {
+            mousePos = hit.point;
+            mousePos.z = z_coord;
+            if (hit.collider.tag == "Point")
+            {
+                GrabScript grab = hit.collider.gameObject.GetComponent<GrabScript>();
+                if (grab != null)
+                {
+                    if (grab.Grabbed)
+                    {
+                        hit.collider.gameObject.transform.position = mousePos;
+                    }
+                }
+            }
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            z_coord -= 0.1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            z_coord += 0.1f;
+        }
+        Mathf.Clamp(z_coord, min_z, max_z);
+    }
+}
--- Assets/TaskManagerScript.cs
+++ Assets/TaskManagerScript.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TaskManagerScript : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class TaskManagerScript : MonoBehaviour
     public int max_active_tasks = 3;
     public List<JobTask> cur_tasks;
     public JobTask selected_task;
+    public List<float> task_times = new List<float>();
+    public TMP_Text TaskText;
     [Serializable]
     public struct JobTask
     {
@@ -38,9 +41,27 @@ public class TaskManagerScript : MonoBehaviour
         {
             cur_tasks.Add(tasks[(int) UnityEngine.Random.Range(0, tasks.Count)]);
         }
+        foreach (JobTask task in cur_tasks)
+        {
+            task_times.Add(task.expiration_time);
+        }
     }
     void Update()
     {
-
+        TaskText.text = "";
+        for (int i = 0; i < task_times.Count; i++)
+        {
+            task_times[i] -= Time.deltaTime;
+            //print("Task time #" + i.ToString() + ": " + task_times[i].ToString());
+            TaskText.text += "Time for Task #" + (i + 1).ToString() + ": " + Mathf.Round(task_times[i]).ToString() + "\n";
+            if (task_times[i] < 0)
+            {
+                task_times.RemoveAt(i);
+                cur_tasks.RemoveAt(i);
+                int new_random = (int)UnityEngine.Random.Range(0, tasks.Count);
+                cur_tasks.Add(tasks[new_random]);
+                task_times.Add(tasks[new_random].expiration_time);
+            }
+        }
     }
 }
